f4e0c614f3690848aca4645538feed0019c66d31
RongalaPravalika <rongalapravalika2006@gmail.com>
2025-02-28 12:03:10 +0530

    Update college.py

--- college.py
+++ college.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
 import google.generativeai as genai
-from sklearn
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TTfidfVectorizer
 
 st.setpage_config(page_title="Svecw College Chatbot", layout="centered")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 csv_url="svcew.details.csv"
 try:
